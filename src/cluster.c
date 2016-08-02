3e163e925f779c64fd9e0e029f3238d4856f63aa
YongMan <yming0221@gmail.com>
2016-08-02 19:21:53 +0800

    don't auto reparent when autofailover switchoff

--- src/cluster.c
+++ src/cluster.c
@@ -1555,7 +1555,8 @@ void clusterUpdateSlotsConfigWith(clusterNode *sender, uint64_t senderConfigEpoc
      *    master.
      * 2) We are a slave and our master is left without slots. We need
      *    to replicate to the new slots owner. */
-    if (newmaster && curmaster->numslots == 0) {
+    /* Disable reparent if autofailover disabled */
+    if (newmaster && curmaster->numslots == 0 && server.cluster_autofailover) {
         redisLog(REDIS_WARNING,
             "Configuration change detected. Reconfiguring myself "
             "as a replica of %.40s", sender->name);
