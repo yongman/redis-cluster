2e0519b94161bb74f9c5f4d9a87287ef72f36916
YongMan <yming0221@gmail.com>
2017-09-27 15:22:05 +0800

    Link jemalloc as dynamic library in wrapper
    avoid jemalloc re-init during dlopen result 4M bytes memroy leak
    
    mentioned in following issues
    * https://github.com/jemalloc/jemalloc/issues/288
    * https://github.com/jemalloc/jemalloc/issues/730

--- src/redis-server-wrap.c
+++ src/redis-server-wrap.c
@@ -3,9 +3,18 @@
 #include <dlfcn.h>
 #include <signal.h>
 
+#include <jemalloc/jemalloc.h>
+
 #include "server.h"
 
 #define LIB_REDIS_PATH "./redis-server.so"
+
+#define malloc(size) je_malloc(size)
+#define calloc(count,size) je_calloc(count,size)
+#define realloc(ptr,size) je_realloc(ptr,size)
+#define free(ptr) je_free(ptr)
+#define mallocx(size,flags) je_mallocx(size,flags)
+#define dallocx(ptr,flags) je_dallocx(ptr,flags)
 
 typedef int (*ENTRY)(int, char **, struct wrapperContext *);
 typedef int (*RELOAD)(int);
@@ -18,14 +27,23 @@ int main(int argc, char ** argv)
     ENTRY entry = NULL;
     RELOAD reload = NULL;
     int ret;
+    char *mem = NULL;
 
     ctx.reload = 0;
 
     strncpy(ctx.hashseed, "1234567890123456", 16);
     ctx.reloadTimes = 0;
 
+    fprintf(stderr, "jemalloc init\n");
+    mem = malloc(1);
+    if (mem == NULL) {
+        fprintf(stderr, "memory alloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    free(mem);
+
     for(;;) {
-        printf("load dynamic library\n");
+        fprintf(stderr, "load dynamic library\n");
         handle = dlopen(LIB_REDIS_PATH, RTLD_LAZY);
         if (!handle) {
             fprintf(stderr, "%s\n", dlerror());
